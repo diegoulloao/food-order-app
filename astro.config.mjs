deb778d744b7664deab9ebce69a7c72735cab1c8
Diego <diegoulloao@icloud.com>
2024-05-26 15:18:07 -0400

    feat: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [
-    svelte(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [svelte(), tailwind({
+    applyBaseStyles: false
+  })],
+  adapter: vercel()
 });
